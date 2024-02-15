f9633f93e59da69dcd622c47f14f8b853c9cec4b
Ali <fahad@gmail.com>
2024-02-15 17:18:50 +0530

    Pattern Questin Version 1 - 15/feb/2024

---
+++ src/Pattern_Questn.java
@@ -0,0 +1,176 @@
+import java.util.*;
+public class Pattern_Questn {
+
+	public static void main(String[] args) {
+		
+		//Pattern -1 
+//		for(int i=0;i<4;i++) {
+//			for(int j=0;j<6;j++) {
+//				System.out.print("0");
+//			}	
+//			System.out.println();
+//		}
+//		000000
+//		000000
+//		000000
+//		000000
+		
+		
+//		for(int i=1;i<5;i++) {
+//			for(int j=0;j<i;j++) {
+//				System.out.print("0");	
+//			}	
+//			System.out.println();
+//		}
+//		0
+//		00
+//		000
+//		0000
+		
+		
+		//Patter - 2
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter input number : ");
+		int num = sc.nextInt();
+//		for(int i=1; i<=num; i++) {
+//			for(int j=0; j<i;j++) {
+//				System.out.print(i+" ");
+//			}
+//			System.out.println();
+//		}
+//		
+//		1 
+//		2 2 
+//		3 3 3 
+//		4 4 4 4 
+		
+//		for(int i=1; i<=num; i++) {
+//			for(int j=0; j<i;j++) {
+//				System.out.print((char)(64+i));
+//			}
+//			System.out.println();
+//		}
+//		
+//		A
+//		BB
+//		CCC
+//		DDDD
+//		EEEEE
+		
+//		for(int i=0; i<num; i++) {
+//			for(int j=i; j<=2*i;j++) {
+//				System.out.print((char)(65+j));
+//			}
+//			System.out.println();
+//		}
+//		A
+//		BC
+//		CDE
+//		DEFG
+//		EFGHI
+		
+//		Pattern -3 
+//		for(int i=0; i< num; i++) {
+//			for(int j=i; j>=0; j--) {
+//				System.out.print((char)(65+num-j-1)+" ");
+//			}
+//			System.out.println();
+//		}
+//		
+//		F 
+//		E F 
+//		D E F 
+//		C D E F 
+//		B C D E F 
+//		A B C D E F 
+		
+//		for(int i=0; i< num; i++) {
+//			for(int j=i; j>=0; j--) {
+//				System.out.print((char)(65+num-j-1)+" ");
+//			}
+//			System.out.println();
+//		}
+//		E 
+//		D E 
+//		C D E 
+//		B C D E 
+//		A B C D E 
+		
+//		Pattern -4
+//		for(int i=0;i<num;i++) {
+//			for(int k=0; k<num-i-1;k++) {
+//				System.out.print(" ");
+//			}	
+//			for(int j=0;j<=i;j++) {
+//				System.out.print(j+1);
+//			}
+//			System.out.println();
+//		}
+//		
+//		1
+//	     12
+//	    123
+//	   1234
+//	  12345
+//	 123456
+//	1234567
+		
+//		for(int i=0; i<num;i++) {
+//			for(int j=i;j<num;j++) {
+//				System.out.print(num-i);
+//			}
+//			System.out.println();
+//		}
+//		
+//		55555
+//		4444
+//		333
+//		22
+//		1
+		
+		
+//		for(int i=0; i<num; i++) {
+//			for(int j=0; j<num-i-1;j++) {
+//				System.out.print(".");
+//			}
+//			for(int k=i; k<=2*i;k++) {
+//				System.out.print(k+1);
+//			}
+//			System.out.println();
+//		}
+//		...1
+//		..23
+//		.345
+//		4567
+		
+
+//		for(int i=0; i<num; i++) {
+//			for(int j=1; j<=num-i-1;j++) {
+//				System.out.print(".");
+//			}
+//			for(int j=i+1; j<=2*i+1;j++) {
+//				System.out.print(j);
+//			}
+//			for(int j=2*i; j>= i+1; j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+//		....1
+//		...232
+//		..34543
+//		.4567654
+//		567898765
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
